0adaace40ea0ba23bcdb8eae48a101f7968abfcc
Priscila Andreani <priscilaandreani@gmail.com>
2020-12-27 15:21:02 -0300

    (navegacaoTab): adiciona script que permite a navegação na lista de livros

---
+++ navegacaoTab.js
@@ -0,0 +1,17 @@
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+if (tabContent.length && tabMenu.length) {
+  tabContent[0].classList.add('ativo');
+
+  function activeTab(index) {
+    tabContent.forEach((section) => section.classList.remove('ativo'));
+    tabContent[index].classList.add('ativo');
+  }
+
+  tabMenu.forEach((section, index) =>
+    section.addEventListener('click', () => {
+      activeTab(index);
+    }),
+  );
+}
