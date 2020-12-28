9cb6f7985a11e10d8fdd771d6791bc163193cc10
Priscila Andreani <priscilaandreani@gmail.com>
2020-12-28 14:10:33 -0300

    (accordionList) adiciona efeito acordeão no FAQ

---
+++ accordionList.js
@@ -0,0 +1,15 @@
+const accordionList = document.querySelectorAll('.js-accordion dt');
+
+if (accordionList.length) {
+  const activeClass = 'ativo';
+  accordionList[0].nextElementSibling.classList.add(activeClass);
+
+  function activeAccordion() {
+    console.log(this);
+    const proximoItem = this.nextElementSibling;
+    proximoItem.classList.toggle(activeClass);
+    console.log(proximoItem);
+  }
+
+  accordionList.forEach((item) => item.addEventListener('click', activeAccordion));
+}
