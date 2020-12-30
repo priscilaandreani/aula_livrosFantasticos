1ef0fd1571eda3a5763b34aa9f02848eff8ea898
Priscila Andreani <priscilaandreani@gmail.com>
2020-12-30 13:51:08 -0300

    (softScroll) adiciona efeito de softScroll a página

---
+++ softScroll.js
@@ -0,0 +1,15 @@
+const menuInterno = document.querySelectorAll('.js-menu ul li a[href^="#"]');
+console.log(menu);
+
+function softScroll(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+  section.scrollIntoView;
+  ({
+    behavior: 'smooth',
+    block: 'start',
+  });
+}
+
+menuInterno.forEach((link) => link.addEventListener('click', softScroll));
