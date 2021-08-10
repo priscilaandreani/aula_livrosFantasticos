3923bcbae3c0bdc947180fd65c2ec0c700d9bf05
priscila andreani <priscilaandreani@gmail.com>
2021-08-10 15:23:55 -0300

    modal: adiciona modal de login

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,10 @@ import initScrollSuave from './modules/softScroll.js'
 import scrollAnimation from './modules/scrollAnimation.js';
 import initAccordion from './modules/accordionList.js';
 import initTabNavigation from './modules/navegacaoTab.js';
+import initModal from './modules/modal.js'
 
 initScrollSuave();
 scrollAnimation();
 initAccordion();
 initTabNavigation();
+initModal();
