ecf0d67fedfdea5ba7d84974a88e0495beb64b0a
priscila andreani <priscilaandreani@gmail.com>
2021-08-10 16:54:46 -0300

    tooltip: adiciona tooltip que segue o mouse

---
+++ scripts/modules/tooltip.js
@@ -0,0 +1,44 @@
+export default function initTooltip(){
+
+  const tooltips = document.querySelectorAll('[data-tooltip]');
+
+  tooltips.forEach(item =>{
+    item.addEventListener('mouseover', onMouseOver)
+  });
+
+  function onMouseOver(event){ 
+    const tooltipBox = createTooltip(this);
+
+    onMouseMove.tooltipBox = tooltipBox;
+    this.addEventListener('mouseover', onMouseMove);
+    onMouseLeave.tooltipBox = tooltipBox;
+    onMouseLeave.element = this;
+    this.addEventListener('mouseleave', onMouseLeave);
+  }
+
+  const onMouseLeave = {
+    handleEvent(){
+      this.tooltipBox.remove();
+      this.element.removeEventListener('mouseleave', onMouseLeave);
+      this.element.removeEventListener('onmousemove', onMouseMove);
+    }
+  }
+
+  const onMouseMove = {
+    handleEvent(event){
+      this.tooltipBox.style.top = event.pageY + 20 + 'px';
+      this.tooltipBox.style.left = event.pageX  + 20 + 'px';
+    }
+  }
+
+  function createTooltip(element){
+    const tooltipBox = document.createElement('div');
+    const text = element.getAttribute('aria-label');
+    tooltipBox.classList.add('tooltip');
+    tooltipBox.innerHTML = text;
+    document.body.appendChild(tooltipBox);
+
+    return tooltipBox;
+  }
+}
+
--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,11 @@ import scrollAnimation from './modules/scrollAnimation.js';
 import initAccordion from './modules/accordionList.js';
 import initTabNavigation from './modules/navegacaoTab.js';
 import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js';
 
 initScrollSuave();
 scrollAnimation();
 initAccordion();
 initTabNavigation();
 initModal();
+initTooltip();
